da9bd4e0ba60009c818342e2255eb6c83b64c303
Gabriela Rivero <regaby@gmail.com>
2021-09-29 15:44:33 -0300

    [UPD] EE invoice fields

--- l10n_ar_pos_einvoice_ticket/static/src/js/pos_model.js
+++ l10n_ar_pos_einvoice_ticket/static/src/js/pos_model.js
@@ -14,6 +14,9 @@ odoo.define('l10n_ar_pos_einvoice_ticket', function (require) {
         'country_id',
     ]);
 
+    var pos_model = require("point_of_sale.models");
+    var SuperPosModel = pos_model.PosModel.prototype;
+
     const PaymentScreen = require('point_of_sale.PaymentScreen');
     const { useListener } = require('web.custom_hooks');
     const Registries = require('point_of_sale.Registries');
@@ -50,125 +53,84 @@ odoo.define('l10n_ar_pos_einvoice_ticket', function (require) {
     pos_model.PosModel = pos_model.PosModel.extend({
 
         _flush_orders: function(orders, options) {
+
             var self = this;
             var result, data;
             result = data = SuperPosModel._flush_orders.call(this,orders, options)
             _.each(orders,function(order){
-
                 if (order.to_invoice)
-                 var order = self.env.pos.get_order();
-                //if (this.env.pos.config.receipt_invoice_number)
-                    data.then(function(order_server_id){
+                var order = self.env.pos.get_order();
+                data.then(function(order_server_id){
+                    var domain = [['pos_reference', '=', order['name']]]
+                    var fields = ['account_move'];
 
 
-                            rpc.query({
-                            model: 'pos.order',
-                            method: 'read',
-                            domain: [['pos_reference', '=', order['name']]],
-                            fields: ['account_move'],
-                            args:[order_server_id, ['account_move','company_id']]
-                                }).then(function(result_dict){
+                    rpc.query({
+                        model: 'pos.order',
+                        method: 'search_read',
+                        args: [domain, fields],
+                    }).then(function(output){
+                        var invoice_number = output[0]['account_move'][1].split(" ")[1];
+                        var invoice_letter = output[0]['account_move'][1].split(" ")[0].substring(3, 4);
+                        self.get_order().invoice_number = invoice_number;
+                        self.get_order().invoice_letter = invoice_letter;
+                        var account_move = output[0]['account_move'][0]
+                        rpc.query({
+                            model: 'account.move',
+                            method: 'search_read',
+                            args: [[['id', '=', account_move]], ['l10n_ar_afip_auth_code',
+                                                               'l10n_ar_afip_auth_code_due',
+                                                               'l10n_ar_afip_qr_code',
+                                                               'l10n_latam_document_type_id',
+                                                               ]],
+                           }
 
-                                    if(result_dict.length){
-
-                                        let invoice = result_dict[0].account_move;
-                                        self.get_order().invoice_id = invoice[1];
-                                        account_move = result_dict[0]['account_move'][0];
-                                        company_id=result_dict[0]['company_id'][0];
-
-                                    }
-                            }).then(function (einvoices) {
-
-
-                                     rpc.query({
-                                         model: 'account.move',
-                                         method: 'search_read',
-                                         args: [[['id', '=', account_move]], ['l10n_ar_afip_auth_code',
-                                                                            'l10n_ar_afip_auth_code_due',
-                                                                            'l10n_ar_afip_barcode',
-                                                                            'l10n_ar_afip_qr_code',
-                                                                            'invoice_date_due',
-                                                                            'l10n_latam_document_type_id',
-                                                                            'company_id',
-                                                                            ]],
-                                        }
-
-                                     ).then(function (einvoice) {
-
-                                        if (account_move>0) {
-                                         l10n_ar_afip_auth_code = einvoice[0]['l10n_ar_afip_auth_code'];
-                                         l10n_ar_afip_qr_code= einvoice[0]['l10n_ar_afip_qr_code'];
-                                         l10n_ar_afip_auth_code_due= einvoice[0]['l10n_ar_afip_auth_code_due'];
-                                         //l10n_latam_document_type_id = einvoice[0]['l10n_latam_document_type_id'];
-                                         l10n_latam_document_type_id = einvoice[0]['l10n_latam_document_type_id'][1].split(" ")[0];
-                                         invoice_date_due= einvoice[0]['invoice_date_due'];
-                                         var split_invoice_date_due=invoice_date_due.split('-');
-                                         invoice_date_due=split_invoice_date_due[2]+"-"+split_invoice_date_due[1]+"-"+split_invoice_date_due[0];
-                                         company_id=einvoice[0]['company_id'][0];
-                                         }
-
-                                        }).then(function (company) {
-
-                                          rpc.query({
-                                          model: 'res.company',
-                                          method: 'search_read',
-                                          args: [[['id', '=', company_id]], ['l10n_ar_afip_start_date','l10n_ar_gross_income_number','street', 'city', 'state_id', 'country_id', 'company_registry']],
-                                    }).then(function (company_partner) {
-                                        l10n_ar_afip_start_date=company_partner[0]['l10n_ar_afip_start_date'];
-                                            if(l10n_ar_afip_start_date!=false){
-                                                var split_l10n_ar_afip_start_date=l10n_ar_afip_start_date.split('-');
-                                                l10n_ar_afip_start_date = split_l10n_ar_afip_start_date[2]+"-"+split_l10n_ar_afip_start_date[1]+"-"+split_l10n_ar_afip_start_date[0];
-                                              }
-                                        l10n_ar_gross_income_number=company_partner[0]['l10n_ar_gross_income_number'];
-                                        street=company_partner[0]['street'];
-                                        city=company_partner[0]['city'];
-
-                                    })
-
-
-                                        })
-
-
-                                 }).catch(function(error){
-                                return result
-                            })
+                        ).then(function (invoices) {
+                            self.get_order().l10n_ar_afip_qr_code = invoices[0]['l10n_ar_afip_qr_code'];
+                            self.get_order().l10n_ar_afip_auth_code = invoices[0]['l10n_ar_afip_auth_code'];
+                            self.get_order().l10n_ar_afip_auth_code_due = invoices[0]['l10n_ar_afip_auth_code_due'];
+                            self.get_order().l10n_latam_document_type_id = invoices[0]['l10n_latam_document_type_id'][1].split(" ")[0];
+                        });
+                    }).catch(function(error){
+                        return result
                     })
+                })
             })
             return result
 
         },
 
     })
-    pos_model.Order = pos_model.Order.extend({
-        export_for_printing: function(){
-            var self = this
-            var receipt = SuperOrder.export_for_printing.call(this)
-            if(self.invoice_id){
-                var invoice_id = self.invoice_id
-                var invoice = invoice_id.split("(")[0]
-                var invoice_number ="";
-                var invoice_letter="";
-                invoice_number = invoice_id.split("(")[0]
-                invoice_letter = invoice_id.split("(")[0].substring(3, 4);
-                //invoice_letter = orders[0]['account_move'][1].split(" ")[0].substring(3, 4);
+    // pos_model.Order = pos_model.Order.extend({
+    //     export_for_printing: function(){
+    //         // var self = this
+    //         // var receipt = SuperOrder.export_for_printing.call(this)
+    //         // if(self.invoice_id){
+    //         //     var invoice_id = self.invoice_id
+    //         //     var invoice = invoice_id.split("(")[0]
+    //         //     var invoice_number ="";
+    //         //     var invoice_letter="";
+    //         //     invoice_number = invoice_id.split("(")[0]
+    //         //     invoice_letter = invoice_id.split("(")[0].substring(3, 4);
+    //         //     //invoice_letter = orders[0]['account_move'][1].split(" ")[0].substring(3, 4);
 
-                receipt.street=street
-                receipt.city=city
-                receipt.invoice_id = invoice
-                receipt.invoice_number = invoice_number
-                receipt.invoice_letter = invoice_letter
-                receipt.l10n_ar_afip_auth_code=l10n_ar_afip_auth_code
-                receipt.l10n_ar_afip_qr_code=l10n_ar_afip_qr_code
-                receipt.l10n_ar_afip_auth_code_due=l10n_ar_afip_auth_code_due
-                receipt.l10n_latam_document_type_id=l10n_latam_document_type_id
-                receipt.invoice_date_due = invoice_date_due
-                receipt.l10n_ar_afip_start_date=l10n_ar_afip_start_date
-                receipt.l10n_ar_gross_income_number = l10n_ar_gross_income_number
+    //         //     receipt.street=street
+    //         //     receipt.city=city
+    //         //     receipt.invoice_id = invoice
+    //         //     receipt.invoice_number = invoice_number
+    //         //     receipt.invoice_letter = invoice_letter
+    //         //     receipt.l10n_ar_afip_auth_code=l10n_ar_afip_auth_code
+    //         //     receipt.l10n_ar_afip_qr_code=l10n_ar_afip_qr_code
+    //         //     receipt.l10n_ar_afip_auth_code_due=l10n_ar_afip_auth_code_due
+    //         //     receipt.l10n_latam_document_type_id=l10n_latam_document_type_id
+    //         //     receipt.invoice_date_due = invoice_date_due
+    //         //     receipt.l10n_ar_afip_start_date=l10n_ar_afip_start_date
+    //         //     receipt.l10n_ar_gross_income_number = l10n_ar_gross_income_number
 
 
-            }
-            return receipt
-        }
-    })
+    //         // }
+    //         // return receipt
+    //     }
+    // })
 
 });
